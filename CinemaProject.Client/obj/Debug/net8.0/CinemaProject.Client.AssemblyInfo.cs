4f41171ff0c32713e0dc174104ef44a074708d56
AlunoAlcidesMaya <aluno@alcidesmaya.com.br>
2024-08-07 16:21:47 -0300

    update minimo

--- CinemaProject.Client/obj/Debug/net8.0/CinemaProject.Client.AssemblyInfo.cs
+++ CinemaProject.Client/obj/Debug/net8.0/CinemaProject.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaProject.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30713def0b273deabc437e812251bf961c25b37f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d56225e745dcd711f453674b0731ac3f84b5241d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaProject.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaProject.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
